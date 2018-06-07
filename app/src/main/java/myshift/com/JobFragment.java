58b2bb01cd97969ebc19f686c005a3e6d73684be
Tony Gorb <30474304+tonygorb@users.noreply.github.com>
2018-06-07 20:22:50 +0300

    Spinners are working now !

--- app/src/main/java/myshift/com/JobFragment.java
+++ app/src/main/java/myshift/com/JobFragment.java
@@ -1,5 +1,6 @@
 package myshift.com;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,8 +8,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class JobFragment extends Fragment {
+public class JobFragment extends Fragment implements AdapterView.OnItemSelectedListener {
+
+    private Spinner spnrBonusHours, spnrBreaks;
 
     private static final String TAG = "JobFragment";
 
@@ -17,6 +25,29 @@ public class JobFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.job_fragment,container,false);
 
+        spnrBonusHours = view.findViewById(R.id.spnr_bonus_hours);
+
+        ArrayAdapter bonusHoursAdapter = ArrayAdapter.createFromResource(getContext(),R.array.bonusHours,android.R.layout.simple_spinner_item);
+        spnrBonusHours.setAdapter(bonusHoursAdapter);
+        spnrBonusHours.setOnItemSelectedListener(this);
+
+
+        spnrBreaks = view.findViewById(R.id.spnr_breaks);
+
+        ArrayAdapter breaksAdapter = ArrayAdapter.createFromResource(getContext(),R.array.breaks,android.R.layout.simple_spinner_item);
+        spnrBreaks.setAdapter(breaksAdapter);
+        spnrBreaks.setOnItemSelectedListener(this);
+
         return view;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        TextView myText = (TextView) view;
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
