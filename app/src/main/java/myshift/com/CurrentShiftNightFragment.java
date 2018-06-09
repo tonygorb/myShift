f9a14749397f40262a68bf9d92d8fd69f012e738
antony <anton240991@mail.ru>
2018-06-10 01:11:03 +0300

    good luck

--- app/src/main/java/myshift/com/CurrentShiftNightFragment.java
+++ app/src/main/java/myshift/com/CurrentShiftNightFragment.java
@@ -1,6 +1,7 @@
 package myshift.com;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.SystemClock;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -9,47 +10,54 @@ import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Chronometer;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
-public class CurrentShiftNightFragment extends Fragment {
+public class CurrentShiftNightFragment extends Fragment implements View.OnClickListener {
 
     private static final String TAG = "CurrentShiftNightFragment";
+    private TextView newShiftDate, startTime, endTime, totalHours,currentTime;
+    private Button startShiftButton , btnSwitch;
+    private Calendar calendar, startCalendar, endCalendar;
+    private SimpleDateFormat simpleDateFormat, simpleTimeFormat , simpleCurrentTimeFormat;
+    private boolean isStartedShift = true;
+    public static String shiftDate, shiftStartTime, shiftEndTime;
 
-    private TextView tvTime, tvDate, btnSwitch;
-    private SimpleDateFormat mSimpleDateFormatTime;
-    private Calendar mCalendar;
-    private String time, date;
-    private Chronometer timer;
-    private ImageButton btnStart;
-
-    private boolean running;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.current_shift_night_fragment,container,false);
 
-        tvTime = view.findViewById(R.id.night_time);
-        tvDate = view.findViewById(R.id.night_date);
-        btnSwitch = view.findViewById(R.id.night_tv_shift);
-        timer = view.findViewById(R.id.night_cron_timer);
-        btnStart = view.findViewById(R.id.night_btn_start_shift);
+        newShiftDate =  view.findViewById(R.id.startDateTV);
+        startTime = view.findViewById(R.id.startTimeTV);
+        endTime = view.findViewById(R.id.endTimeTV);
+        totalHours = view.findViewById(R.id.total);
+        currentTime = view.findViewById(R.id.currentTime);
 
-        mCalendar = Calendar.getInstance();
-        mSimpleDateFormatTime = new SimpleDateFormat("HH:mm");
-        time = mSimpleDateFormatTime.format(mCalendar.getTime());
+        startShiftButton = view.findViewById(R.id.startShiftButton);
 
-        date = new SimpleDateFormat("MMM ,yyyy", Locale.getDefault()).format(new Date());
+        calendar = Calendar.getInstance();
+        simpleDateFormat = new SimpleDateFormat("dd/MM/yy");
+        simpleTimeFormat = new SimpleDateFormat("HH:mm:ss a");
+        simpleCurrentTimeFormat = new SimpleDateFormat("HH:mm:ss");
+        shiftDate = simpleDateFormat.format(calendar.getTime());
+        currentTime.setText(simpleCurrentTimeFormat.format(calendar.getTime()));
 
-        tvTime.setText(time);
-        tvDate.setText(date);
+        newShiftDate.setText(shiftDate);
+
+
+
+        startShiftButton.setOnClickListener(this);
+
 
         btnSwitch.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -62,35 +70,56 @@ public class CurrentShiftNightFragment extends Fragment {
             }
         });
 
-        btnStart.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View view) {
-                chronometer(view);
 
-                if (running){
-                    btnSwitch.setEnabled(false);
-                    running = true;
-                } else {
-                    btnSwitch.setEnabled(true);
-                    running = false;
-                }
-                return true;
-            }
-        });
 
         return view;
     }
 
-    public void chronometer(View view) {
-        if (!running){
-            timer.setBase(SystemClock.elapsedRealtime());
-            timer.start();
-            running = true;
-        } else if (running){
-            timer.stop();
-            timer.setBase(SystemClock.elapsedRealtime());
-            running = false;
+    public void calculatedHours() throws ParseException {
+        Date startShift = null;
+        Date endShift = null;
+
+        try {
+            startShift = simpleTimeFormat.parse(shiftStartTime);
+            endShift = simpleTimeFormat.parse(shiftEndTime);
+
+            long diff = endShift.getTime() - startShift.getTime();
+
+            long diffSeconds = diff / 1000 % 60;
+            long diffMinutes = diff / (60 * 1000) % 60;
+            long diffHours = diff / (60 * 60 * 1000) % 24;
+
+            totalHours.setText("Total Shift Time" + ": " + diffHours + ":" + diffMinutes + ":" + diffSeconds);
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        return;
+
+
     }
+
+    public void onClick(View v) {
+        if (isStartedShift){
+            startCalendar = Calendar.getInstance();
+            shiftStartTime = simpleTimeFormat.format(startCalendar.getTime());
+            startTime.setText(shiftStartTime);
+            startShiftButton.setText("STOP");
+            isStartedShift = false;
+            return;
+        }
+
+        if (!isStartedShift ) {
+            endCalendar = Calendar.getInstance();
+            shiftEndTime = simpleTimeFormat.format(endCalendar.getTime());
+            endTime.setText(shiftEndTime);
+            startShiftButton.setText("START");
+            try {
+                calculatedHours();
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
+
