74cdd6e9ef05a58062ebee1e034afa9a2977a935
Tony Gorb <30474304+tonygorb@users.noreply.github.com>
2018-06-09 14:36:17 +0300

    Date is shown correctly on "Shifts" fragment * Hebrew is the default language for the calendar. * Next & Previous buttons work correctly.
    * Add & Delete button were initialized but not implemented yet.

--- app/src/main/java/myshift/com/ShiftsFragment.java
+++ app/src/main/java/myshift/com/ShiftsFragment.java
@@ -7,16 +7,74 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class ShiftsFragment extends Fragment {
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
-    private static final String TAG = "CurrentShiftFragment";
+public class ShiftsFragment extends Fragment implements View.OnClickListener {
+
+    private static final String TAG = "ShiftsFragment";
+
+    TextView date;
+    Calendar currentDate = Calendar.getInstance();
+    int mYear = currentDate.get(Calendar.YEAR);
+    int mMonth = currentDate.get(Calendar.MONTH);
+    int mDay = currentDate.get(Calendar.DAY_OF_MONTH);
+    SimpleDateFormat dateFormat = new SimpleDateFormat("MMM, yyyy",Locale.forLanguageTag("he"));
+
+    String dateTime = new SimpleDateFormat("MMM ,yyyy", Locale.forLanguageTag("he")).format(new Date());
+
+    private Button prev, next, add, delete;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.shifts_fragment,container,false);
 
+        date = view.findViewById(R.id.tv_shifts_date);
+        date.setText(dateTime);
+
+        prev = view.findViewById(R.id.shifts_btn_prev);
+        prev.setOnClickListener(this);
+
+        next = view.findViewById(R.id.shifts_btn_next);
+        next.setOnClickListener(this);
+
+        add = view.findViewById(R.id.shifts_btn_add);
+        add.setOnClickListener(this);
+
+        delete = view.findViewById(R.id.shifts_btn_delete);
+        delete.setOnClickListener(this);
+
         return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.shifts_btn_prev:
+                currentDate.add(Calendar.MONTH,-1);
+                dateTime = dateFormat.format(currentDate.getTime());
+                date.setText(dateTime);
+                break;
+
+            case R.id.shifts_btn_next:
+                currentDate.add(Calendar.MONTH,+1);
+                dateTime = dateFormat.format(currentDate.getTime());
+                date.setText(dateTime);
+                break;
+
+            case R.id.shifts_btn_add:
+
+                break;
+
+            case R.id.shifts_btn_delete:
+
+                break;
+        }
     }
 }
